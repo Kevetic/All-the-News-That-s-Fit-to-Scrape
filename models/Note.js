523e03ec0b263df8866789b5f9ed921d2f8e0006
Kevetic <kevetic@outlook.com>
2020-01-23 14:05:32 -0600

    decided to try to remake it with a lesser boilerplate

--- models/Note.js
+++ models/Note.js
@@ -1,17 +1,13 @@
-var mongoose = require("mongoose");
+var mongoose = require ("mongoose");
 
-// Save a reference to the Schema constructor
 var Schema = mongoose.Schema;
 
-// Using the Schema constructor, create a new NoteSchema object
-// This is similar to a Sequelize model
-var NoteSchema = new Schema({
-  // `noteText` is of type String
-  noteText: String
+varNoteSchema = new Schema({
+title:String,
+body:String
+
 });
 
-// This creates our model from the above schema, using mongoose's model method
-var Note = mongoose.model("Note", NoteSchema);
+var Note = mongoose.model("Note", varNoteSchema);
 
-// Export the Note model
 module.exports = Note;
--- models/index.js
+++ models/index.js
@@ -2,3 +2,4 @@ module.exports = {
     Article: require("./Article"),
     Note: require("./Note")
   };
+  
